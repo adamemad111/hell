5c4badb0a9115d1f8903e3210f3edc45575a0918
adamemad111 <top.fashion.reviewer@gmail.com>
2024-12-16 10:05:35 +0800

    Update yellow-pages.py

--- yellow-pages.py
+++ yellow-pages.py
@@ -314,11 +314,11 @@ def process_hardcoded_file(file_path, column_name):
         return
 
     # Extract relevant columns
-    urls = data[column_name].dropna().tolist()[1249:1300]
-    industries = data.get('Industry', pd.Series(['Unknown'] * len(data))).tolist()[1249:1300]
-    job_titles = data.get('Job Title', pd.Series(['Unknown'] * len(data))).tolist()[1249:1300]
+    urls = data[column_name].dropna().tolist()[1299:]
+    industries = data.get('Industry', pd.Series(['Unknown'] * len(data))).tolist()[1299:]
+    job_titles = data.get('Job Title', pd.Series(['Unknown'] * len(data))).tolist()[[1299:]
     # Process each URL
-    for idx, (url, industry, job_title) in enumerate(zip(urls, industries, job_titles), start=1250):
+    for idx, (url, industry, job_title) in enumerate(zip(urls, industries, job_titles), start=1300):
         print(f"Processing URL {idx}/{len(urls)}: {url}")
 
         clue_match = re.search(r'clue=([^&]*)', url)  # Extract the 'clue' value
