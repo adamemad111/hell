e3f76a0dde3838a9f592b34da478ef65795fc489
adamemad111 <top.fashion.reviewer@gmail.com>
2024-12-23 14:46:48 +0800

    Update yellow-pages.py

--- yellow-pages.py
+++ yellow-pages.py
@@ -333,13 +333,13 @@ def process_sheet2(file_path, column_name='Yellow-Pages-Link', sheet_name='Retai
         return
 
     # Extract relevant columns
-    urls = data[column_name].dropna().tolist()[199:300]
-    categories = data['Category'].tolist()[199:300]
-    sub_categories = data['Sub-category'].tolist()[199:300]
-    keywords = data['Suggested Keyword'].tolist()[199:300]
+    urls = data[column_name].dropna().tolist()[299:400]
+    categories = data['Category'].tolist()[299:400]
+    sub_categories = data['Sub-category'].tolist()[299:400]
+    keywords = data['Suggested Keyword'].tolist()[299:400]
 
     # Process each URL
-    for idx, (url, category, sub_category, keyword) in enumerate(zip(urls, categories, sub_categories, keywords), start=200):
+    for idx, (url, category, sub_category, keyword) in enumerate(zip(urls, categories, sub_categories, keywords), start=300):
         print(f"Processing URL {idx}/{len(urls)}: {url}")
 
         clue_match = re.search(r'clue=([^&]*)', url)
